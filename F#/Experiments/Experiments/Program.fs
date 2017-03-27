3e31809bc05dc695086af4adce68002d644e9295
Will Mortl <Will Mortl>
2017-03-27 14:22:55 -0700

    accept JSON

---
+++ F#/Experiments/Experiments/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+module Map
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let a = System.Console.ReadLine
+    let b = a()
+    printfn "%s" b
+    let c = a()
+    let d = [1,2,3,4]
+    let e = d.map
+    0 // return an integer exit code
